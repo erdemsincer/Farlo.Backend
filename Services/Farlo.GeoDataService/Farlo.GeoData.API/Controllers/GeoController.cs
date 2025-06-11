7676552ca61e89cbd2c191d242c69cbbf808cd47
Erdem <erdemsincer123@icloud.com>
2025-06-11 22:11:42 +0300

    servisler

---
+++ Services/Farlo.GeoDataService/Farlo.GeoData.API/Controllers/GeoController.cs
@@ -0,0 +1,32 @@
+﻿using Farlo.EventContracts.Geo;
+using Farlo.GeoData.Application.DTOs;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Farlo.GeoData.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GeoController : ControllerBase
+{
+    private readonly IPublishEndpoint _publishEndpoint;
+
+    public GeoController(IPublishEndpoint publishEndpoint)
+    {
+        _publishEndpoint = publishEndpoint;
+    }
+
+    [HttpPost("query")]
+    public async Task<IActionResult> PublishGeoQuery([FromBody] GeoQueryRequestDto dto)
+    {
+        var requestId = Guid.NewGuid().ToString();
+
+        await _publishEndpoint.Publish(new GeoQueryRequestedEvent(
+            requestId,
+            dto.Latitude,
+            dto.Longitude
+        ));
+
+        return Ok(new { Message = "Event published", RequestId = requestId });
+    }
+}
---
+++ Services/Farlo.GeoDataService/Farlo.GeoData.Application/Interfaces/IGeoDataService.cs
@@ -0,0 +1,6 @@
+﻿namespace Farlo.GeoData.Application.Interfaces;
+
+public interface IGeoDataService
+{
+    Task<(string Climate, string Vegetation, string Elevation)> GetGeoDataAsync(double latitude, double longitude);
+}
