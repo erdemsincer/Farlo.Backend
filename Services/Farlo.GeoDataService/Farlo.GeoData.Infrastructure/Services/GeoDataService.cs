f950266e24cda54dfc9d86cc5eca6782498f5941
Erdem <erdemsincer123@icloud.com>
2025-06-11 22:11:48 +0300

    servis ayarları

---
+++ Services/Farlo.GeoDataService/Farlo.GeoData.Infrastructure/Services/GeoDataService.cs
@@ -0,0 +1,16 @@
+﻿using Farlo.GeoData.Application.Interfaces;
+
+namespace Farlo.GeoData.Infrastructure.Services;
+
+public class GeoDataService : IGeoDataService
+{
+    public Task<(string Climate, string Vegetation, string Elevation)> GetGeoDataAsync(double latitude, double longitude)
+    {
+        
+        return Task.FromResult((
+            "Karasal iklim",
+            "Bozkır",
+            "1045m"
+        ));
+    }
+}
