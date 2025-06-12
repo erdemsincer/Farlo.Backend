0ffaf089bde4d84ebd8fd38a9214ef6f927d5913
Erdem <erdemsincer123@icloud.com>
2025-06-13 00:35:20 +0300

    entites

---
+++ Services/Farlo.InsightService/Farlo.Insight.Domain/Entities/Insight.cs
@@ -0,0 +1,9 @@
+﻿namespace Farlo.Insight.Domain.Entities;
+
+public class Insight
+{
+    public Guid Id { get; set; }
+    public string RequestId { get; set; }
+    public string Summary { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
