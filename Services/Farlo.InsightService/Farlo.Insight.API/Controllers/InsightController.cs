a1dd8cd8d8d671ee9725648761abf0c322be47f9
Erdem <erdemsincer123@icloud.com>
2025-06-13 00:35:26 +0300

    controller

---
+++ Services/Farlo.InsightService/Farlo.Insight.API/Controllers/InsightController.cs
@@ -0,0 +1,23 @@
+﻿using Farlo.Insight.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Farlo.Insight.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class InsightController : ControllerBase
+{
+    private readonly IInsightRepository _repository;
+
+    public InsightController(IInsightRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var insights = await _repository.GetAllAsync();
+        return Ok(insights);
+    }
+}
