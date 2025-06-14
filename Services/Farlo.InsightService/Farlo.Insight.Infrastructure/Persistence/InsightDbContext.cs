b777299531e9a6662a9484da575f36a93efeffe9
Erdem <erdemsincer123@icloud.com>
2025-06-14 17:22:21 +0300

    contex

---
+++ Services/Farlo.InsightService/Farlo.Insight.Infrastructure/Persistence/InsightDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Farlo.Insight.Domain.Entities;
+using MassTransit;
+using System.Collections.Generic;
+
+namespace Farlo.Insight.Infrastructure.Persistence;
+
+public class InsightDbContext : DbContext
+{
+    public InsightDbContext(DbContextOptions<InsightDbContext> options) : base(options) { }
+
+    public DbSet<Domain.Entities.Insight> Insights => Set<Domain.Entities.Insight>();
+}
