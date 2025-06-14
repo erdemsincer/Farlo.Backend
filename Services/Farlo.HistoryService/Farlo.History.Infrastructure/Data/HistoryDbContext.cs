57e49ae5b287907f18a765901bbcd92b8d8f4954
Erdem <erdemsincer123@icloud.com>
2025-06-14 17:51:01 +0300

    contex

---
+++ Services/Farlo.HistoryService/Farlo.History.Infrastructure/Data/HistoryDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Farlo.History.Domain.Entities;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Farlo.History.Infrastructure.Data;
+
+public class HistoryDbContext : DbContext
+{
+    public HistoryDbContext(DbContextOptions<HistoryDbContext> options) : base(options) { }
+
+    public DbSet<GeoQueryHistory> GeoQueryHistories => Set<GeoQueryHistory>();
+}
