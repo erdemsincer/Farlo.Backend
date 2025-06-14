98b8a13a63bbbde958e51750942fee306a8f07eb
Erdem <erdemsincer123@icloud.com>
2025-06-14 17:51:13 +0300

    consumer

---
+++ Services/Farlo.HistoryService/Farlo.History.Infrastructure/Messaging/Consumers/GeoQueryCompletedConsumer.cs
@@ -0,0 +1,31 @@
+﻿using Farlo.EventContracts.Geo;
+using Farlo.History.Application.Interfaces;
+using Farlo.History.Domain.Entities;
+using MassTransit;
+
+namespace Farlo.History.Infrastructure.Messaging.Consumers;
+
+public class GeoQueryCompletedConsumer : IConsumer<GeoQueryCompletedEvent>
+{
+    private readonly IHistoryRepository _repository;
+
+    public GeoQueryCompletedConsumer(IHistoryRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Consume(ConsumeContext<GeoQueryCompletedEvent> context)
+    {
+        var message = context.Message;
+
+        var history = new GeoQueryHistory
+        {
+            RequestId = message.RequestId,
+            Latitude = message.Latitude,
+            Longitude = message.Longitude,
+            CreatedAt = DateTime.UtcNow
+        };
+
+        await _repository.SaveAsync(history);
+    }
+}
