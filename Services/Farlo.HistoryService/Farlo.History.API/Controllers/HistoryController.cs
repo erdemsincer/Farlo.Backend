999dc6c963874d16bb27eafcbd670bb2a5075bfd
Erdem <erdemsincer123@icloud.com>
2025-06-14 17:50:47 +0300

    history controller

---
+++ Services/Farlo.HistoryService/Farlo.History.API/Controllers/HistoryController.cs
@@ -0,0 +1,23 @@
+﻿using Farlo.History.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Farlo.History.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HistoryController : ControllerBase
+{
+    private readonly IHistoryRepository _repository;
+
+    public HistoryController(IHistoryRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var all = await _repository.GetAllAsync();
+        return Ok(all);
+    }
+}
