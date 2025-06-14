21a72735190563022a063972ee1f4779cce90819
Erdem <erdemsincer123@icloud.com>
2025-06-14 17:51:20 +0300

    repositories

---
+++ Services/Farlo.HistoryService/Farlo.History.Infrastructure/Repositories/PostgresHistoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using Farlo.History.Application.Interfaces;
+using Farlo.History.Domain.Entities;
+using Farlo.History.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Farlo.History.Infrastructure.Repositories;
+
+public class PostgresHistoryRepository : IHistoryRepository
+{
+    private readonly HistoryDbContext _context;
+
+    public PostgresHistoryRepository(HistoryDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task SaveAsync(GeoQueryHistory history)
+    {
+        _context.GeoQueryHistories.Add(history);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<GeoQueryHistory>> GetAllAsync()
+    {
+        return await _context.GeoQueryHistories
+            .OrderByDescending(h => h.CreatedAt)
+            .ToListAsync();
+    }
+}
